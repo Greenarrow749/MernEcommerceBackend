4f5b8e35cb284d5e786adc015e78ca18c79158ee
AasthaPrasad <pdaastha@gmail.com>
2023-01-29 21:23:20 +0530

    changed header to setHeader in app.js

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ app.use(cors({
   optionSuccessStatus:200
 }));
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', true);
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
 });
 
